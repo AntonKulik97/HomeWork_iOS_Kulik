f6d91a2682f8d8184eb8263a53fcf691a2df59d5
Антон Кулик <antonio5398883@gmail.com>
2022-05-18 22:41:32 +0300

    Add HW 4

---
+++ HW_4_i23_BeerBar/HW_4_i23_BeerBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_4_i23_BeerBar
+//
+//  Created by Антон Кулик on 18.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
